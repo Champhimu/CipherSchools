0c1034af5ecc01c716f079bb9c7383dfe1542701
Himanshu Thakur <champhimu359@gmail.com>
2023-04-23 22:40:30 +0530

    baseurl updated

--- src/utility/appdata.js
+++ src/utility/appdata.js
@@ -1,6 +1,7 @@
 const appdata = {
     // baseUrl: "https://playwebbackend.onrender.com",
-    baseUrl: "http://localhost:8000",
+    baseUrl: "https://cipherschools-2bfj.onrender.com/",
+    // baseUrl: "http://localhost:8000",
     // baseUrl: "http://192.168.43.14:8000"
 }
 export const userInfo = {
